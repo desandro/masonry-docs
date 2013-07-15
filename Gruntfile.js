2698145f9059c78cff5747cc3a0469f3f9730a1e
David DeSandro <desandrocodes@gmail.com>
2013-07-14 20:56:27 -0400

    first pass and building in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,32 +14,32 @@ module.exports = function( grunt ) {
   grunt.initConfig({
 
     jshint: {
-      docs: [ 'js/controller.js', 'js/*/*.js',  ],
+      docs: [ 'js/controller.js', 'js/*/*.js'  ],
       options: grunt.file.readJSON('js/.jshintrc')
     },
 
     concat: {
       js: {
         src: [ 'js/controller.js', 'js/pages/*.js' ],
-        dest: 'js/masonry-docs.js'
+        dest: 'build/js/masonry-docs.js'
       },
       pkgd: {
         // src will be set in package-sources task
-        dest: 'masonry.pkgd.js',
+        dest: 'build/masonry.pkgd.js',
         options: {
           banner: banner
         }
       },
       css: {
         src: [ 'bower_components/normalize-css/normalize.css', 'css/*.css', '!css/masonry-docs.css' ],
-        dest: 'css/masonry-docs.css'
+        dest: 'build/css/masonry-docs.css'
       }
     },
 
     uglify: {
       pkgd: {
         files: {
-          'masonry.pkgd.min.js': [ 'masonry.pkgd.js' ]
+          'build/masonry.pkgd.min.js': [ 'build/masonry.pkgd.js' ]
         },
         options: {
           banner: banner
@@ -47,7 +47,7 @@ module.exports = function( grunt ) {
       },
       js: {
         files: {
-          // 'js/masonry-site.min.js' will be set in bower-list-map
+          // 'build/js/masonry-site.min.js' will be set in bower-list-map
         }
       }
     },
@@ -56,7 +56,7 @@ module.exports = function( grunt ) {
     hbarz: {
       docs: {
         files: {
-          './': '_content/*'
+          'build/': '_content/*'
         },
         options: {
           templates: '_templates/*.mustache',
@@ -65,16 +65,70 @@ module.exports = function( grunt ) {
       }
     },
 
+    // ----- copy ----- //
+    copy: {
+      "public": {
+        files: [
+          {
+            expand: true, // enable dynamic options
+            cwd: 'public/', // set cwd, excludes it in build path
+            src: [ '**', '!.htaccess' ],
+            dest: 'build/'
+          }
+        ]
+      },
+      css: {
+        files: [
+          {
+            expand: true, // enable dynamic options
+            cwd: 'css/', // set cwd, excludes it in build path
+            src: [ '*' ],
+            dest: 'build/css/'
+          }
+        ]
+      },
+      js: {
+        files: [
+          {
+            expand: true, // enable dynamic options
+            cwd: 'js/', // set cwd, excludes it in build path
+            src: [ '**' ],
+            dest: 'build/js/'
+          }
+        ]
+      },
+      bowerSources: {
+        // additional sources will be set in bower-list-map
+        // friggin Nicolas, not using main the right way :P
+        src: [ 'components/jquery/jquery.min.js' ],
+        dest: 'build/'
+      }
+    },
+
+
     watch: {
       content: {
         files: [ '_content/*', '_templates/*.mustache' ],
         tasks: [ 'bower-list-map', 'hbarz' ]
+      },
+      "public": {
+        files: [ 'public/**' ],
+        tasks: [ 'copy:public' ]
+      },
+      css: {
+        files: [ 'css/*' ],
+        tasks: [ 'copy:css' ]
+      },
+      js: {
+        files: [ 'js/**' ],
+        tasks: [ 'copy:js' ]
       }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -87,7 +141,8 @@ module.exports = function( grunt ) {
     'package-sources',
     'concat',
     'uglify',
-    'hbarz'
+    'hbarz',
+    'copy'
   ]);
 
 };
