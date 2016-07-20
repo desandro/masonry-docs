862de5b818ade44fa0ddafc02fda0eabe6f8e618
David DeSandro <desandrocodes@gmail.com>
2016-07-20 13:28:51 -0400

    📝 add stagger option

---
+++ modules/stagger/stagger.js
@@ -0,0 +1,16 @@
+MD.modules.stagger = function( elem ) {
+  'use strict';
+
+  var grid = elem.querySelector('.grid');
+
+  var msnry = new Masonry( grid, {
+    columnWidth: 80,
+    stagger: 30,
+  });
+
+  filterBindEvent( grid, 'click', '.grid-item', function( event ) {
+    event.target.classList.toggle('grid-item--gigante');
+    msnry.layout();
+  });
+
+};
