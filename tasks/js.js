cf16db33e9f3b4b9dc137d3b8f9110de221dff75
Dave DeSandro <desandrocodes@gmail.com>
2023-10-23 14:47:14 -0400

    Move to vercel; nvmrc 10; add package-lock.json
    remove uglify in js task

--- tasks/js.js
+++ tasks/js.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var getGlobPaths = require('./utils/get-glob-paths');
 
 var jsSrc = [
@@ -26,7 +25,6 @@ var jsSrc = [
 // concat & minify js
 gulp.task( 'docs-js', function() {
   gulp.src( jsSrc )
-    .pipe( uglify() )
     .pipe( concat('masonry-docs.min.js') )
     .pipe( gulp.dest('build/js') );
 });
